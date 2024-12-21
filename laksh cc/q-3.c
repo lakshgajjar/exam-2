eb5d336db8df6042a441b73fcb0a77720aeb76b7
lakshgajjar <lakshgajjar667@gmail.com>
2024-12-21 10:33:08 +0530

    Add files via upload

---
+++ laksh cc/q-3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,square;
+    printf("Enter the value of a:");
+    scanf("%d" ,  &a);
+    square= a*a;
+    printf("square of %d = %d", a,square);
+    return 0;
+}
---
+++ laksh cc/q-4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int cha;
+    for(cha='a'; cha<='z';cha++);
+    {
+        printf("%c",cha);
+    }
+    printf("\n");
+    return 0;
+}
